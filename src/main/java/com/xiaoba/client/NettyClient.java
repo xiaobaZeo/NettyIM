0692f67a1d21f7dfa20424ddb711c446a91f1902
xiaobazeo <xiaobazeo@gmail.com>
2023-03-22 01:18:53 +0800

    client retry

--- src/main/java/com/xiaoba/client/NettyClient.java
+++ src/main/java/com/xiaoba/client/NettyClient.java
@@ -6,13 +6,19 @@ package com.xiaoba.client;
  * @Description : 客户端的启动
  */
 
+import com.sun.org.apache.regexp.internal.RE;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class NettyClient {
+    private static final int MAX_RETRY = 5;
+
     public static void main(String[] args) {
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
         Bootstrap bootstrap = new Bootstrap();
@@ -29,11 +35,30 @@ public class NettyClient {
                     }
                 });
         //建立连接，通过.addListener监听是否连接成功，因为connect异步、返回一个future
-        bootstrap.connect("juejin.im", 80).addListener(future -> {
+//        bootstrap.connect("juejin.im", 80).addListener(future -> {
+//            if (future.isSuccess()) {
+//                System.out.println("连接成功！");
+//            } else {
+//                System.err.println("连接失败!");
+//            }
+//        });
+        connect(bootstrap, "juejin,im", 80, MAX_RETRY);
+    }
+
+    //失败重连机制函数
+    private static void connect(Bootstrap bootstrap, String host, final int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
             if (future.isSuccess()) {
-                System.out.println("连接成功！");
+                System.out.println("连接成功");
+            } else if (retry == 0) {
+                System.err.println("重试次数用完，放弃连接！");
             } else {
-                System.err.println("连接失败!");
+                //第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                //本次重连的间隔
+                int delay = 1 << order;
+                System.out.println(new Date() + "连接失败，第" + order + "次重连...");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
             }
         });
     }
